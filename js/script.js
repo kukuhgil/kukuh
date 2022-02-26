aad94b3db789068f268aab9d67ca193edd87360d
kukuhgil <75466382+kukuhgil@users.noreply.github.com>
2022-02-27 00:45:56 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var pages = new Array('halaman1', 'halaman2');
+
+function klikMenu() {
+  document.getElementsByClassName('newpage')[0].classList.toggle('buka_menu');
+}
+
+function PindahKeHalaman(page) {
+  var newpage = document.getElementsByClassName('newpage')[0];
+  var sections = document.getElementsByClassName('dalemnya_halaman');
+  for (i = 0; i < sections.length; i++) {
+    sections[i].classList.remove('before', 'after');
+    if (i > page) {
+      sections[i].classList.add('after');
+    }
+  }
+  newpage.classList.add('page-' + pages[page]);
+}
